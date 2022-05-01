f0ca13ae0e5c7f2aed47aa052decad118ac59251
DigitalIceCream <john-philipp.vogt@mailbox.org>
2022-05-01 23:27:09 +0300

    First Commit of current programs

---
+++ Part1/p1ch2.c
@@ -0,0 +1,17 @@
+/**
+Author: 	John-Philipp Vogt
+Date:		30.04.2022
+Synopsis:	A simple program that uses the Characters, Integers, and Floating-Point Data Types.
+**/
+
+#include <stdio.h>
+
+int main ()
+{
+	printf("I am learning the %c programming language\n", 'C');
+	printf("I have just completed Chapter %d\n", 2);
+	printf("I am %.2f percent ready to move on \n", 99.);
+	printf("to the next chapter!\n");
+
+	return 0;
+}
