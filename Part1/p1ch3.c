f5b240bf4f047ccb4c0e125986cc03deef66cd0d
DigitalIceCream <john-philipp.vogt@mailbox.org>
2022-05-01 23:37:33 +0300

    Added p1ch3.c

---
+++ Part1/p1ch3.c
@@ -0,0 +1,11 @@
+/*
+Author: 	John-Philipp Vogt
+Date:		01.05.2022
+Synopsis:	A simple non-program to use some comments.
+*/
+#include <stdio.h>
+
+int main()
+{
+	printf("I like these new comments!\n"); // Do in not?!
+}
