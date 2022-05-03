c4ce128972c7b30e8613566d8eb1757c953791d4
DigitalIceCream <john-philipp.vogt@mailbox.org>
2022-05-03 20:36:23 +0300

    Deleted compiled files, added p1ch5.

---
+++ Part1/p1ch4.c
@@ -0,0 +1,14 @@
+/*
+Author:		John-Philipp Vogt
+Date:		02.05.2022
+Synopsis:	A simple program using printf() and some common escape sequences.
+*/
+
+#include <stdio.h>
+main()
+{
+    printf("Just one small step for coders. One giant leap for");
+    printf("programmers!\n");
+    return 0;
+
+}
