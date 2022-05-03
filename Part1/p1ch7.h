aea14dfc033aaf7f5c925bfdb76aeec8a093b60a
DigitalIceCream <john-philipp.vogt@mailbox.org>
2022-05-03 22:06:29 +0300

    Fixed some typos.

--- Part1/p1ch7.h
+++ Part1/p1ch7.h
@@ -1,15 +1,15 @@
 /*
 Author:		John-Philipp Vogt
 Date:		03.05.2022
-Synopsis:	A program demonstrating the use of #include and #define (preprocessor directive and NOT comments!).
+Synopsis:	A header file demonstrating the use of #include and #define (preprocessor directive and NOT comments!).
 */
 
 #include <stdio.h> // essentially a file-merge command, allows for the use of the printf() function e.g..
-		   // built-in files are called with <filename>
+		   // built-in files are called with <filename>, custom ones with "filename"
 		   // technically a header file thus the '.h' ending
 // if you want to have a certain value that will not change, or changes only rarely,
 // you can use the #DEFINE statement to set those, before the main(){} function.
-// if you need them in multiple programs, you can place the in a header file like:
+// if you need them in multiple programs, you can place them in a header file like:
 
 #define KIDS 3
 #define FAMILY "The McFlys"
