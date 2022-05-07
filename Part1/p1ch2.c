037da7659a8ce6012b38ba279febd9285c0d6ab3
DigitalIceCream <john-philipp.vogt@mailbox.org>
2022-05-07 22:57:20 +0300

    Some minor edits!

--- Part1/p1ch2.c
+++ Part1/p1ch2.c
@@ -8,9 +8,9 @@ Synopsis:	A simple program that uses the Characters, Integers, and Floating-Poin
 
 int main ()
 {
-	printf("I am learning the %c programming language\n", 'C');
-	printf("I have just completed Chapter %d\n", 2);
-	printf("I am %.2f percent ready to move on \n", 99.);
+	printf("I am learning the %c programming language.\n", 'C'); // '%c' for char - type
+	printf("I have just completed Chapter %d.\n", 2); // '%d' for int - type
+	printf("I am %.2f percent ready to move on \n", 99.95); // '%f' for floating point - type (NB: .2 determines the digits after the period)
 	printf("to the next chapter!\n");
 
 	return 0;
--- Part1/p1ch4.c
+++ Part1/p1ch4.c
@@ -2,13 +2,29 @@
 Author:		John-Philipp Vogt
 Date:		02.05.2022
 Synopsis:	A simple program using printf() and some common escape sequences.
+Filename:	p1ch4.c
 */
 
 #include <stdio.h>
-main()
+int main()
 {
     printf("Just one small step for coders. One giant leap for");
-    printf("programmers!\n");
-    return 0;
+    printf("programmers!\n\n\n");
 
+    // more code to help with escape sequences and conversion characters
+
+    printf("Quantity\tCost\tTotal\n");
+    printf("%d\t\t%.2f\t$%.2f\n\n\n", 3, 9.99, 29.97);
+    printf("Too many spaces    \b\b\b\b can be fixed with the ");
+    printf("\\%c Escape character\n", 'b');
+    printf("\n\a\n\a\n\a\n\aSkip a few lines, and beep ");
+    printf("a few beeps. \n\n\n");
+    printf("%s %c.", "You are kicking butt at learning", 'C');
+    printf("You just finished chapter %d. \nYou have finished ", 4);
+    printf("%.f%c of the book!", 12.5000, '%');
+    printf("\n\nOne third equals %.2f or ", 0.333333);
+    printf("%.3f or %.4f or ", 0.333333, 0.333333);
+    printf("%.5f or %.6f\n\n\n", 0.333333, 0.333333);
+
+    return 0;
 }
